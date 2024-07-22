2d8d42009c0a07629b790df4a0c3d735ea0598a0
Happy Makadiya <happy.m@simformsolutions.com>
2024-07-22 23:08:07 +0530

    Env and Keystore Fixes

--- lib/values/env.dart
+++ lib/values/env.dart
@@ -5,5 +5,5 @@ part 'env.g.dart';
 @Envied()
 abstract class Env {
   @EnviedField(varName: 'OPEN_AI_SECRET', obfuscate: false)
-  static String gptKey = '';
+  static String gptKey = _Env.gptKey;
 }
